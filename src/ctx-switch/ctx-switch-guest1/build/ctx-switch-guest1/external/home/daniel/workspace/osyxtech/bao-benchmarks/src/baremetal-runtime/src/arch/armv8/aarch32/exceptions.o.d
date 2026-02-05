1698599e99844a541b5f28b18ff1b0b3ba2de58e
Daniel Oliveira <drawnpoetry@gmail.com>
2026-02-05 17:14:48 +0000

    ref(build): remove build artifacts
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/ctx-switch/ctx-switch-guest1/build/ctx-switch-guest1/external/home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime/src/arch/armv8/aarch32/exceptions.o.d
+++
@@ -1,4 +0,0 @@
-/home/daniel/workspace/osyxtech/bao-benchmarks/src/ctx-switch/ctx-switch-guest1/build/ctx-switch-guest1/external//home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/aarch32/exceptions.o: \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/aarch32/exceptions.S \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/inc/sysregs.h \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime/src/arch/armv8//aarch32/inc/arch/subarch/sysregs.h
--- src/irq-lat-wce/irq-lat-wce-guest1/build/irq-lat-wce-guest1/external/home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime/src/arch/armv8/aarch32/exceptions.o.d
+++
@@ -1,4 +0,0 @@
-/home/daniel/workspace/osyxtech/bao-benchmarks/src/irq-lat-wce/irq-lat-wce-guest1/build/irq-lat-wce-guest1/external//home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/aarch32/exceptions.o: \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/aarch32/exceptions.S \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime//src/arch/armv8/inc/sysregs.h \
- /home/daniel/workspace/osyxtech/bao-benchmarks/src/baremetal-runtime/src/arch/armv8//aarch32/inc/arch/subarch/sysregs.h
